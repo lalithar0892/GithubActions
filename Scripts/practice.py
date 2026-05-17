eec9c2b1f09066a39d60eefa006269006712b37f
Lalitha082002 <lalithar0892@gmail.com>
2026-05-17 12:18:38 +0530

    Added sys import

--- Scripts/practice.py
+++ Scripts/practice.py
@@ -3,4 +3,11 @@ num = int(sys.argv[1])
 for i in range(1,num+1):
     print(f"{i} : HIIIIII")
 
+with open("report.txt", "w") as file:
+    file.write("Daily Build Report\n")
+    file.write("-------------------\n")
+    file.write("Build Status : SUCCESS\n")
+    file.write("Tests Passed : 25\n")
+    file.write("Tests Failed : 0\n")
 
+print("Report Generated Successfully")
